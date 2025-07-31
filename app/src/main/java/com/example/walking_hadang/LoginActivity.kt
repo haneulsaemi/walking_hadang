c8775b832f25552b20a8f35dabf9cad90aaa9314
HaSam <ssam8610@naver.com>
2025-08-01 00:29:06 +0900

    [FEAT] 주요 기능 화면 생성 및 로그인 구현

---
+++ app/src/main/java/com/example/walking_hadang/LoginActivity.kt
@@ -0,0 +1,108 @@
+package com.example.walking_hadang
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.walking_hadang.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.GoogleAuthProvider
+
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        if(MyApplication.checkAuth()){
+            changeVisibility("login")
+        }else{
+            changeVisibility("logout")
+        }
+        val requestLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            try{
+                val account = task.getResult(ApiException::class.java)
+                val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                MyApplication.auth.signInWithCredential(credential)
+                    .addOnCompleteListener(this){task ->
+                        if(task.isSuccessful && MyApplication.checkAuth()){
+                            MyApplication.email = account.email
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }else{
+                            changeVisibility("logout")
+                        }
+                    }
+            }catch (e: ApiException){
+                changeVisibility("logout")
+            }
+        }
+
+        binding.goSignInBtn.setOnClickListener {
+            //회원가입
+            changeVisibility("signin")
+        }
+        binding.googleLoginBtn.setOnClickListener{
+            //구글 로그인
+            val gso = GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("3728742656-bolckl5h3ifi1dknlu308unecpe2jgpt.apps.googleusercontent.com")
+                .requestEmail()
+                .build()
+
+            val signInIntent = GoogleSignIn.getClient(this, gso).signInIntent
+            requestLauncher.launch(signInIntent)
+        }
+    }
+
+    fun changeVisibility(mode: String){
+        if(mode === "login"){
+            binding.run {
+                goSignInBtn.visibility= View.GONE
+                googleLoginBtn.visibility= View.GONE
+                kakaoLoginBtn.visibility = View.GONE
+                authEmailEditView.visibility= View.GONE
+                authPasswordEditView.visibility= View.GONE
+                signBtn.visibility= View.GONE
+                loginBtn.visibility= View.GONE
+            }
+
+        }else if(mode === "logout"){
+            binding.run {
+                goSignInBtn.visibility = View.VISIBLE
+                googleLoginBtn.visibility = View.VISIBLE
+                kakaoLoginBtn.visibility = View.VISIBLE
+                authEmailEditView.visibility = View.VISIBLE
+                authPasswordEditView.visibility = View.VISIBLE
+                signBtn.visibility = View.GONE
+                loginBtn.visibility = View.VISIBLE
+            }
+        }else if(mode === "signin"){
+            binding.run {
+                goSignInBtn.visibility = View.GONE
+                googleLoginBtn.visibility = View.GONE
+                kakaoLoginBtn.visibility = View.GONE
+                authEmailEditView.visibility = View.VISIBLE
+                authPasswordEditView.visibility = View.VISIBLE
+                signBtn.visibility = View.VISIBLE
+                loginBtn.visibility = View.GONE
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/walking_hadang/MainActivity.kt
+++ app/src/main/java/com/example/walking_hadang/MainActivity.kt
@@ -1,20 +1,106 @@
 package com.example.walking_hadang
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.walking_hadang.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        binding.toolbar.setTitle("워킹하당")
+        binding.toolbar.setTitleTextColor(Color.parseColor("#ffffff"))
+
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_fragment_container, HomeFragment())
+            .commit()
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> switchFragment(HomeFragment())
+                R.id.nav_recoding -> switchFragment(RecodingFragment())
+                R.id.nav_map -> switchFragment(MapFragment())
+                R.id.nav_community -> switchFragment(CommunityFragment())
+            }
+            true
         }
+    }
+
+    fun switchFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_fragment_container, fragment)
+            .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.menu_login -> {
+                MyApplication.auth.signOut()
+                MyApplication.email = null
+
+                val intent = Intent(this, LoginActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+                true
+            }
+//            R.id.menu_alarm_settings ->{
+//                switchFragment(AlarmSettingsFragment())
+//                true
+//            }
+//            R.id.menu_setting ->{
+//                val intent = Intent(this, SettingActivity::class.java)
+//                startActivity(intent)
+//                true
+//            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        val item = menu?.findItem(R.id.menu_login)
+        val user = menu?.findItem(R.id.menu_user)
+
+        val currentUser = MyApplication.auth.currentUser
+        if (currentUser != null && currentUser.isEmailVerified) {
+            MyApplication.email = currentUser.email
+            item?.title = "Logout"
+            user?.isVisible = true
+            user?.title = MyApplication.email
+        }
+        else {
+            item?.title = "Login"
+            user?.isVisible = false
+        }
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        invalidateOptionsMenu()
+    }
+
+    override fun onResume() {
+        super.onResume()
     }
 }
